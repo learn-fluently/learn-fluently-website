56a4bc368f1f51bd231bfaf2ba2d93b4501b6bdd
Amir Khorsandi <khorsandi@me.com>
2019-08-31 17:23:09 +0200

    Update privacy policy

--- src/components/pages/privacy-policy/PrivacyPolicyViewModel.ts
+++ src/components/pages/privacy-policy/PrivacyPolicyViewModel.ts
@@ -7,9 +7,9 @@ export class PrivacyPolicyViewModel {
 
     public get privacyPolicy(): string[] {
         return [
-            `- Learn Fluently app doesn't collect any data, it just uses the data at the moment that you use the app and by removing the app all the data will be permanently removed.`,
-            `- This app doesn't send any data to any server, all the data process locally in your phone.`,
-            `- Recognizing spoken words happens by using the Speech framework, this is a framework by Apple and it's part of iOS, you can find more details in the <a href="https://developer.apple.com/documentation/speech">documentation</a>.`,
+            `- Learn Fluently doesn't collect any data, it just uses the data at the moment that you use the app and by removing the app all the data will be permanently removed.`,
+            `- This app doesn't send any data to any server, all the data is processed locally in your iPhone.`,
+            `- Recognizing spoken words implemented by using the Speech framework, this is a framework by Apple and it's part of iOS, you can find more details in the <a href="https://developer.apple.com/documentation/speech">documentation</a>.`,
             `- This is an open-source software, so you can look into the code on Github and see how it works.`,
         ];
     }
